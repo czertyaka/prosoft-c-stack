3e1fbb385039b6ef0dc47979b28403453392e377
o.pyhov <pyhovoleg31@gmail.com>
2022-11-16 11:25:20 +0500

    cstack stubs

--- cstack.c
+++ cstack.c
@@ -1,2 +1,24 @@
 #include "cstack.h"
+#include <stddef.h>
+
+stack_t* stack_new()
+{
+    return NULL;
+}
+
+void stack_free(stack_t*)
+{}
+
+unsigned int stack_size(stack_t*)
+{
+    return 0;
+}
+
+void stack_push(stack_t*, const void*, const unsigned int)
+{}
+
+unsigned int stack_pop(stack_t*, void*, const unsigned int)
+{
+    return 0;
+}
 
--- cstack.h
+++ cstack.h
@@ -11,7 +11,7 @@ struct node
 typedef struct node node_t;
 typedef node_t* stack_t;
 
-node_t ** stack_new();
+stack_t* stack_new();
 void stack_free(stack_t* list);
 unsigned int stack_size(stack_t* stack);
 void stack_push(stack_t* stack, const void* data_in, const unsigned int size);
