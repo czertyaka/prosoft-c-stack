72230ce1b75de433fde566521827458cc982697c
o.pyhov <o.pyhov@prosoftsystems.ru>
2022-11-19 21:23:06 +0500

    renamed typedef

--- cstack.c
+++ cstack.c
@@ -1,23 +1,23 @@
 #include "cstack.h"
 #include <stddef.h>
 
-stack_t* stack_new()
+cstack_t* stack_new()
 {
     return NULL;
 }
 
-void stack_free(stack_t*)
+void stack_free(cstack_t*)
 {}
 
-unsigned int stack_size(stack_t*)
+unsigned int stack_size(cstack_t*)
 {
     return 0;
 }
 
-void stack_push(stack_t*, const void*, const unsigned int)
+void stack_push(cstack_t*, const void*, const unsigned int)
 {}
 
-unsigned int stack_pop(stack_t*, void*, const unsigned int)
+unsigned int stack_pop(cstack_t*, void*, const unsigned int)
 {
     return 0;
 }
--- cstack.h
+++ cstack.h
@@ -9,12 +9,12 @@ struct node
 };
 
 typedef struct node node_t;
-typedef node_t* stack_t;
+typedef node_t* cstack_t;
 
-stack_t* stack_new();
-void stack_free(stack_t* list);
-unsigned int stack_size(stack_t* stack);
-void stack_push(stack_t* stack, const void* data_in, const unsigned int size);
-unsigned int stack_pop(stack_t* stack, void* data_out, const unsigned int size);
+cstack_t* stack_new();
+void stack_free(cstack_t* list);
+unsigned int stack_size(cstack_t* stack);
+void stack_push(cstack_t* stack, const void* data_in, const unsigned int size);
+unsigned int stack_pop(cstack_t* stack, void* data_out, const unsigned int size);
 
 #endif /* CSTACK_H */
