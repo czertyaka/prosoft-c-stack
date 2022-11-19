14ce464684401a592090a7f7599db004fe0c48d0
o.pyhov <o.pyhov@prosoftsystems.ru>
2022-11-19 23:23:03 +0500

    fix typo

--- cstack.h
+++ cstack.h
@@ -2,7 +2,7 @@
 #define CSTACK_H
 
 void* stack_new();
-void stack_free(void* list);
+void stack_free(void* stack);
 unsigned int stack_size(void* stack);
 void stack_push(void* stack, const void* data_in, const unsigned int size);
 unsigned int stack_pop(void* stack, void* data_out, const unsigned int size);
