d55fdb155a7bce5c0b448cb56da06421664bdb3d
o.pyhov <o.pyhov@prosoftsystems.ru>
2022-11-20 19:57:26 +0500

    unused variable macro

--- cstack.c
+++ cstack.c
@@ -1,6 +1,8 @@
 #include "cstack.h"
 #include <stddef.h>
 
+#define UNUSED(VAR) (void)(VAR)
+
 struct node
 {
     const struct node* prev;
@@ -18,33 +20,33 @@ hstack_t stack_new()
 
 void stack_free(const hstack_t hstack)
 {
-    (void)hstack;
+    UNUSED(hstack);
 }
 
 int stack_valid_handler(const hstack_t hstack)
 {
-    (void)hstack;
+    UNUSED(hstack);
     return 1;
 }
 
 unsigned int stack_size(const hstack_t hstack)
 {
-    (void)hstack;
+    UNUSED(hstack);
     return 0;
 }
 
 void stack_push(const hstack_t hstack, const void* data_in, const unsigned int size)
 {
-    (void)hstack;
-    (void)data_in;
-    (void)size;
+    UNUSED(hstack);
+    UNUSED(data_in);
+    UNUSED(size);
 }
 
 unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int size)
 {
-    (void)hstack;
-    (void)data_out;
-    (void)size;
+    UNUSED(hstack);
+    UNUSED(data_out);
+    UNUSED(size);
     return 0;
 }
 
