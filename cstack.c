926476c9b8f7e9e3548c29411afca947a35ac7ae
ilya-talankin <ilua0821@gmail.com>
2022-11-29 00:11:10 +0500

    New architecture work normally

--- cstack.c
+++ cstack.c
@@ -37,7 +37,7 @@ typedef struct top_node_table {
     struct node* p_top_node;
 } stack_t;
 
-stack_t* handlers_table; 
+stack_t** handlers_table; 
 int stacks_count = 0;
 
 // Functions:
@@ -48,23 +48,24 @@ hstack_t stack_new()
     printf("stacks_count = %d\n", stacks_count);
     // search for empty space
     for (int i = 0; i < stacks_count; i++) {
-        if (handlers_table[i].reserved == 0) {
+        if (handlers_table[i] -> reserved == 0) {
             printf("Cell %d is empty\n", i);
-            handlers_table[i].reserved = 1;
-            handlers_table[i].p_top_node = NULL;
+            handlers_table[i] -> reserved = 1;
+            handlers_table[i] -> p_top_node = NULL;
             return i;
         }
     }
     // array extension
-    handlers_table = (stack_t*) realloc(handlers_table, (stacks_count + 1) * sizeof(stack_t*));
+    handlers_table = realloc(handlers_table, (stacks_count + 1) * sizeof(handlers_table));
     if (handlers_table == NULL) { //checking for correct memory allocation
-        printf("Allocation error\n");
+        printf("Reallocation error\n");
         return -1;
     }
     ++ stacks_count;
     handler = stacks_count - 1;
-    handlers_table[handler].reserved = 1;
-    handlers_table[handler].p_top_node = NULL;
+    handlers_table[handler] = malloc(sizeof(stack_t*));
+    handlers_table[handler] -> reserved = 1;
+    handlers_table[handler] -> p_top_node = NULL;
     printf("Array extension, returned %d\n", handler);
     return handler;
 }
@@ -73,30 +74,30 @@ void stack_free(const hstack_t hstack) {
     if (stack_valid_handler(hstack) != 0) {
         return;
     }
-    if (handlers_table[hstack].p_top_node == NULL) {
-        handlers_table[hstack].reserved = 0;
+    if (handlers_table[hstack] -> p_top_node == NULL) {
+        handlers_table[hstack] -> reserved = 0;
         return;
     } //добавить дефрагментацию
-    while (handlers_table[hstack].p_top_node != NULL) {
-        const struct node* prev_node = handlers_table[hstack].p_top_node -> prev;
+    while (handlers_table[hstack] -> p_top_node != NULL) {
+        const struct node* prev_node = handlers_table[hstack] -> p_top_node -> prev;
         //НЕ РАБОТАЕТ ОСВОБОЖДЕНИЕ ПАМЯТИ!!!!!!!!!!!!!!
         //free(handlers_table[hstack].p_top_node -> data); 
         //free(handlers_table[hstack].p_top_node);
-        handlers_table[hstack].p_top_node = prev_node;
+        handlers_table[hstack] -> p_top_node = prev_node;
     }
-    handlers_table[hstack].reserved = 0;
+    handlers_table[hstack] -> reserved = 0;
 }   
 
 int stack_valid_handler(const hstack_t hstack){
-    if ((hstack >= 0) && (hstack < stacks_count) && (handlers_table[hstack].reserved != 0)){
+    if ((hstack >= 0) && (hstack < stacks_count) && (handlers_table[hstack] -> reserved != 0)){
         return 0;
     } else return 1;
 }
 
 unsigned int stack_size(const hstack_t hstack){   
     if (stack_valid_handler(hstack) == 0){
-        if (handlers_table[hstack].p_top_node != NULL){
-            unsigned int size = handlers_table[hstack].p_top_node -> node_number;
+        if (handlers_table[hstack] -> p_top_node != NULL){
+            unsigned int size = handlers_table[hstack] -> p_top_node -> node_number;
             return size;
         }
         else return 0;
@@ -107,23 +108,23 @@ unsigned int stack_size(const hstack_t hstack){
 
 void stack_push(const hstack_t hstack, const void* data_in, const unsigned int size) {     
     if ((stack_valid_handler(hstack) == 0)&&(data_in != NULL)){
-        struct node* p_prev = handlers_table[hstack].p_top_node; //saving pointer to prev element
+        struct node* p_prev = handlers_table[hstack] -> p_top_node; //saving pointer to prev element
         int new_node_number = 1;
         if (p_prev != NULL) {
             new_node_number = p_prev -> node_number + 1; //increment number of node
         }
-        handlers_table[hstack].p_top_node = (node_t*) malloc(sizeof(node_t*));
-        if (handlers_table[hstack].p_top_node == NULL) { //checking for correct memory allocation
+        handlers_table[hstack] -> p_top_node = (node_t*) malloc(sizeof(node_t*));
+        if (handlers_table[hstack] -> p_top_node == NULL) { //checking for correct memory allocation
             return;
         }
-        handlers_table[hstack].p_top_node -> prev = p_prev;
-        handlers_table[hstack].p_top_node -> node_number = new_node_number;
-        handlers_table[hstack].p_top_node -> data = (char*) malloc (size);
-        if (handlers_table[hstack].p_top_node -> data == NULL) { //checking for correct memory allocation
+        handlers_table[hstack] -> p_top_node -> prev = p_prev;
+        handlers_table[hstack] -> p_top_node -> node_number = new_node_number;
+        handlers_table[hstack] -> p_top_node -> data = (char*) malloc (size);
+        if (handlers_table[hstack] -> p_top_node -> data == NULL) { //checking for correct memory allocation
             return;
         }
-        handlers_table[hstack].p_top_node -> size = size;
-        char *data_out = handlers_table[hstack].p_top_node -> data;
+        handlers_table[hstack] -> p_top_node -> size = size;
+        char *data_out = handlers_table[hstack] -> p_top_node -> data;
         memcpy(data_out, data_in, size);
     }
 }
@@ -134,20 +135,20 @@ unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int
     printf("hstack = %d\n", hstack);
     if ((stack_valid_handler(hstack) == 0)
         &&(data_out != NULL)
-        &&(handlers_table[hstack].p_top_node != NULL)) {
+        &&(handlers_table[hstack] -> p_top_node != NULL)) {
 
         printf("conditions is OK\n");
-        int size_to_copy = (handlers_table[hstack].p_top_node -> size > size) ? size : handlers_table[hstack].p_top_node -> size;
+        int size_to_copy = (handlers_table[hstack] -> p_top_node -> size > size) ? size : handlers_table[hstack] -> p_top_node -> size;
         printf("size_to_copy = %d\n", size_to_copy);
-        char *data_in = handlers_table[hstack].p_top_node -> data;
+        char *data_in = handlers_table[hstack] -> p_top_node -> data;
         memcpy(data_out, data_in, size_to_copy);
         printf("Copied sucsessful\n");
-        const struct node* prev_node = handlers_table[hstack].p_top_node -> prev;
+        const struct node* prev_node = handlers_table[hstack] -> p_top_node -> prev;
         printf("prev is seted\n");
         //НЕ РАБОТАЕТ ОСВОБОЖДЕНИЕ ПАМЯТИ!!!!!!!!!!!!!!
         //free(handlers_table[hstack].p_top_node -> data); 
         //free(handlers_table[hstack].p_top_node);
-        handlers_table[hstack].p_top_node = prev_node;
+        handlers_table[hstack] -> p_top_node = prev_node;
         printf("end of pop\n\n");
         return size_to_copy;
     }
@@ -166,6 +167,7 @@ int main()
     printf("%d\n", stack_new());
     printf("%d\n", stack_new());
     printf("%d\n", stack_new());
+    
 
     //stack valid handler and push test
 
@@ -189,4 +191,6 @@ int main()
     stack_pop(h, &y, 4);
     printf("Popped: %d\n", y);
     return 0;
+
+    stack_push(h, &x, sizeof(x));
 }
