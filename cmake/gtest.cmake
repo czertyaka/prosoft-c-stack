2ff2b31d072e27087f4b22e37148a803b79c08e6
o.pyhov <o.pyhov@prosoftsystems.ru>
2022-11-28 09:51:18 +0500

    gtest.cmake fixes

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -10,46 +10,32 @@ ExternalProject_Add(
     INSTALL_COMMAND ""
     LOG_DOWNLOAD ON
     LOG_CONFIGURE ON
-    LOG_BUILD ON)
+    LOG_BUILD ON
+)
 
 ExternalProject_Get_Property(googletest source_dir)
 set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include)
 set(GMOCK_INCLUDE_DIRS ${source_dir}/googlemock/include)
 
 ExternalProject_Get_Property(googletest binary_dir)
-set(GTEST_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+function(add_gtest_library library)
+    set(LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}${library}${CMAKE_STATIC_LIBRARY_SUFFIX})
+    add_library(${library} UNKNOWN IMPORTED)
+    set_target_properties(
+        ${library} PROPERTIES
+        "IMPORTED_LOCATION" "${LIBRARY_PATH}"
+        "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+    )
+    add_dependencies(${library} googletest)
+endfunction()
+
+add_gtest_library(gtest)
 set(GTEST_LIBRARY gtest)
-add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
-set_target_properties(
-    ${GTEST_LIBRARY} PROPERTIES
-    "IMPORTED_LOCATION" "${GTEST_LIBRARY_PATH}"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
-add_dependencies(${GTEST_LIBRARY} googletest)
-
-set(GTEST_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX})
+add_gtest_library(gtest_main)
 set(GTEST_MAIN_LIBRARY gtest_main)
-add_library(${GTEST_MAIN_LIBRARY} UNKNOWN IMPORTED)
-set_target_properties(
-    ${GTEST_MAIN_LIBRARY} PROPERTIES
-    "IMPORTED_LOCATION" "${GTEST_MAIN_LIBRARY_PATH}"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
-set(GMOCK_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
+add_gtest_library(gmock)
 set(GMOCK_LIBRARY gmock)
-add_library(${GMOCK_LIBRARY} UNKNOWN IMPORTED)
-set_target_properties(
-    ${GMOCK_LIBRARY} PROPERTIES
-    "IMPORTED_LOCATION" "${GMOCK_LIBRARY_PATH}"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
-add_dependencies(${GMOCK_LIBRARY} googlemock)
-
-set(GMOCK_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock_main${CMAKE_STATIC_LIBRARY_SUFFIX})
+add_gtest_library(gmock_main)
 set(GMOCK_MAIN_LIBRARY gmock_main)
-add_library(${GMOCK_MAIN_LIBRARY} UNKNOWN IMPORTED)
-set_target_properties(
-    ${GMOCK_MAIN_LIBRARY} PROPERTIES
-    "IMPORTED_LOCATION" "${GMOCK_MAIN_LIBRARY_PATH}"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
+
