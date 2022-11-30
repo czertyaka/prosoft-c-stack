b629add1923db03cbaf8938649a70762a0cf17f0
ilya-talankin <ilua0821@gmail.com>
2022-11-30 23:25:14 +0500

    Now note_t is not const

--- cstack.c
+++ cstack.c
@@ -2,9 +2,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 typedef struct node {
-    const struct node* prev;
+    struct node* prev;
     unsigned int size;
     char* data;
     unsigned int node_number;
@@ -12,7 +13,7 @@ typedef struct node {
 
 typedef struct top_node_table {
     unsigned short int reserved;
-    struct node* p_top_node;
+    node_t* p_top_node;
 } stack_t;
 
 stack_t** handlers_table; 
@@ -57,10 +58,10 @@ void stack_free(const hstack_t hstack) {
     }
     //delete all elements
     while (handlers_table[hstack] -> p_top_node != NULL) {
-        const node_t* prev_node = handlers_table[hstack] -> p_top_node -> prev;
+        node_t* prev_node = handlers_table[hstack] -> p_top_node -> prev;
         //Правильно ли освобождаю память?
         free(handlers_table[hstack] -> p_top_node -> data); 
-        free(handlers_table[hstack] -> p_top_node);
+        free((void*)handlers_table[hstack] -> p_top_node);
         handlers_table[hstack] -> p_top_node = prev_node;
     }
     handlers_table[hstack] -> reserved = 0;
@@ -95,7 +96,7 @@ unsigned int stack_size(const hstack_t hstack){
 
 void stack_push(const hstack_t hstack, const void* data_in, const unsigned int size) {     
     if ((stack_valid_handler(hstack) == 0)&&(data_in != NULL)){
-        struct node* p_prev = handlers_table[hstack] -> p_top_node; //saving pointer to prev element
+        node_t* p_prev = handlers_table[hstack] -> p_top_node; //saving pointer to prev element
         int new_node_number = 1;
         if (p_prev != NULL) {
             new_node_number = p_prev -> node_number + 1; //increment number of node
@@ -124,11 +125,66 @@ unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int
         int size_to_copy = (handlers_table[hstack] -> p_top_node -> size > size) ? size : handlers_table[hstack] -> p_top_node -> size;
         char *data_in = handlers_table[hstack] -> p_top_node -> data;
         memcpy(data_out, data_in, size_to_copy);
-        const struct node* prev_node = handlers_table[hstack] -> p_top_node -> prev;
+        struct node* prev_node = handlers_table[hstack] -> p_top_node -> prev;
         //Правильно ли освобождаю память?
         free((void*)handlers_table[hstack] -> p_top_node -> data); 
         free((void*)handlers_table[hstack] -> p_top_node);
         handlers_table[hstack] -> p_top_node = prev_node;
         return size_to_copy;
     }
+}
+
+int main()
+{   
+    while(1){
+        char input;
+        printf("\nChoose function:\nn - stack_new\nf - stack free\nv - stack_valid_handler\ns - stack_size\nu - stack_push\no - stack_pop\nq - quit\nYour char: ");
+        scanf("%s", &input);
+
+        if (input == 'n'){
+            printf("%d\n", stack_new());
+        }
+        if (input == 'v'){
+            printf("Enter the handler: ");
+            int h;
+            scanf("%d", &h);
+            if (stack_valid_handler(h) == 0) {
+                printf("Handler is valid\n");
+            } else printf("Handler is NOT valid\n");
+        }
+        if (input == 'u'){
+            printf("Enter the handler: ");
+            int h;
+            scanf("%d", &h);
+            int x;
+            printf("Enter the int to push: ");
+            scanf("%d", &x);
+            stack_push(h, &x, sizeof(x));
+        }
+        if (input == 'o') {
+            printf("Enter the handler: ");
+            int h;
+            scanf("%d", &h);
+            int y;
+            stack_pop(h, &y, 4);
+            printf("Popped: %d\n", y);
+        }
+        if (input == 'f'){
+            printf("Enter the handler: ");
+            int h;
+            scanf("%d", &h);
+            stack_free(h);
+        }
+        if (input == 's'){
+            printf("Enter the handler: ");
+            int h;
+            scanf("%d", &h);
+            printf("%d\n", stack_size(h));
+        }
+
+        if (input == 'q') {
+            return 0;
+        }
+    }
+    return 0;
 }
