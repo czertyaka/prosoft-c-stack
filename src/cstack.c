9705f1c65e345e4fddbb43e0e5eac17eb11472f5
Onishenko-sci <118472680+Onishenko-sci@users.noreply.github.com>
2022-12-02 11:34:21 +0000

    Убрал const, добавил проверки

--- src/cstack.c
+++ src/cstack.c
@@ -2,9 +2,18 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
-hstack_id stack_new()
+typedef int bool_in_c;
+enum
+{
+    false,
+    true
+};
+
+stack_head stacks_head[_MAX_STACKS] = {{((void *)0), 0, true}};
+
+hstack_t stack_new()
 {
     for (int id = 0; id < _MAX_STACKS; id++)
         if (stacks_head[id].is_free)
@@ -17,34 +26,57 @@ hstack_id stack_new()
     return -1;
 }
 
-void stack_free(const hstack_id stack_id)
+void stack_free(const hstack_t stack_id)
 {
-    UNUSED(stack_id);
-    node* iterator = stacks_head[stack_id].head_pointer;
-    for (int i = 0; i < stacks_head[stack_id].size; i++)
+    if (stack_id > _MAX_STACKS || stack_id < 0)
+        return;
+
+    if (stacks_head[stack_id].is_free)
+        return;
+
+    node *iterator = stacks_head[stack_id].head_pointer;
+    for (unsigned int i = 0; i < stacks_head[stack_id].size; i++)
     {
         free(iterator->data);
-        node* temp = iterator;
-        iterator = iterator->prev;
+        node *temp = iterator;
+        node *iterator = iterator->prev;
         free(temp);
     }
+
+    stacks_head[stack_id].head_pointer = NULL;
+    stacks_head[stack_id].is_free = true;
+    stacks_head[stack_id].size = 0;
     return;
 }
 
-int stack_valid_handler(const hstack_id stack_id)
+int stack_valid_handler(const hstack_t stack_id)
 {
+    if (stack_id > _MAX_STACKS || stack_id < 0)
+        return 1;
+
     return stacks_head[stack_id].is_free;
 }
 
-unsigned int stack_size(const hstack_id stack_id)
+unsigned int stack_size(const hstack_t stack_id)
 {
+    if (stack_id > _MAX_STACKS || stack_id < 0)
+        return 0u;
+
+    if (stacks_head[stack_id].is_free)
+        return 0u;
+
     return stacks_head[stack_id].size;
 }
 
-int stack_push(const hstack_id stack_id, const void *data_in, const unsigned int size)
+int stack_push(const hstack_t stack_id, const void *data_in, const unsigned int size)
 {
+    if (stack_id > _MAX_STACKS || stack_id < 0)
+        return 0;
+    if (stacks_head[stack_id].is_free)
+        return 0;
     if (data_in == NULL)
         return 0;
+
 
     node *new_node = malloc(sizeof(node));
     if (new_node == NULL)
@@ -55,27 +87,31 @@ int stack_push(const hstack_id stack_id, const void *data_in, const unsigned int
         return 0;
     new_node->size = size;
 
-    memcpy(new_node->data,data_in,size);
+    memcpy(new_node->data, data_in, size);
 
     new_node->prev = stacks_head[stack_id].head_pointer;
     stacks_head[stack_id].head_pointer = new_node;
-    stacks_head[stack_id].size+= 1;
+    stacks_head[stack_id].size += 1;
     return 1;
 }
 
-unsigned int stack_pop(const hstack_id stack_id, void *data_out, const unsigned int size)
+unsigned int stack_pop(const hstack_t stack_id, void *data_out, const unsigned int size)
 {
+    if (stack_id > _MAX_STACKS || stack_id < 0)
+        return 0;
+    if (stacks_head[stack_id].is_free)
+        return 0;
     if (data_out == NULL)
         return 0;
-     if (size < stacks_head[stack_id].head_pointer->size)
+    if (size < stacks_head[stack_id].head_pointer->size)
         return 0;
     if (stacks_head[stack_id].size == 0)
         return 0;
 
-    memcpy(data_out,stacks_head[stack_id].head_pointer->data,size);
+    memcpy(data_out, stacks_head[stack_id].head_pointer->data, size);
 
     free(stacks_head[stack_id].head_pointer->data);
-    node* temp = stacks_head[stack_id].head_pointer;
+    node *temp = stacks_head[stack_id].head_pointer;
     stacks_head[stack_id].head_pointer = stacks_head[stack_id].head_pointer->prev;
     stacks_head[stack_id].size -= 1;
     free(temp);
--- src/cstack.h
+++ src/cstack.h
@@ -3,35 +3,31 @@
 
 #define _MAX_STACKS 16
 
-typedef int hstack_id;
+typedef int hstack_t;
  
 typedef struct node_t node;
 struct node_t
 {
-    const node* prev;
+    node* prev;
     void* data;
     unsigned int size;
 };
 
-typedef int bool;
-enum { false, true };
 
 typedef struct stack_head_t stack_head;
 struct stack_head_t
 {
     node* head_pointer;
     unsigned int size;
-    bool is_free;
+    int is_free;
 };
 
 
-stack_head stacks_head[_MAX_STACKS] = { ((void*)0), 0, true };
-
-hstack_id stack_new();
-void stack_free(const hstack_id stack);
-int stack_valid_handler(const hstack_id stack);
-unsigned int stack_size(const hstack_id stack);
-int stack_push(const hstack_id stack, const void* data_in, const unsigned int size);
-unsigned int stack_pop(const hstack_id stack, void* data_out, const unsigned int size);
+hstack_t stack_new();
+void stack_free(const hstack_t stack);
+int stack_valid_handler(const hstack_t stack);
+unsigned int stack_size(const hstack_t stack);
+int stack_push(const hstack_t stack, const void* data_in, const unsigned int size);
+unsigned int stack_pop(const hstack_t stack, void* data_out, const unsigned int size);
 
 #endif /* CSTACK_H */
