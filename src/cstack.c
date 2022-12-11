a94e50ff3e34058e1e7adb7bbe5081058d2fd448
Daniil <bezlepkin_d01@mail.ru>
2022-12-11 18:02:08 +0500

    add realization for stack_free() and stack_valid_handler()

--- src/cstack.c
+++ src/cstack.c
@@ -24,11 +24,11 @@ typedef struct stack_table
     struct stack *pTable;
 } stack_table_t;
 
-stack_table_t g_table = {.reservedSize = 16, .lastStack = -1, .pTable = NULL};
+stack_table_t g_table = {.reservedSize = 0, .lastStack = -1, .pTable = NULL};
 
 hstack_t stack_new()
 {
-    if (g_table.lastStack == NULL)
+    if (g_table.lastStack == -1)
     {
         g_table.pTable = (stack_t *)malloc(g_table.reservedSize * sizeof(stack_t));
         if (g_table.pTable == NULL)
@@ -58,12 +58,30 @@ hstack_t stack_new()
 
 void stack_free(const hstack_t hstack)
 {
-
+    if (stack_valid_handler(hstack) == 0)
+    {
+        node_t *currentPointer = g_table.pTable[hstack].top;
+        if (currentPointer != NULL)
+        {
+            node_t *prevPointer = currentPointer->prev;
+            for (int i = 0; i < g_table.pTable[hstack].stackSize; i++)
+            {
+                free(currentPointer);
+                currentPointer = prevPointer;
+                if (currentPointer != NULL)
+                    prevPointer = currentPointer->prev;
+            }
+        }
+        g_table.pTable[hstack].stackSize = -1;
+    }
 }
 
 int stack_valid_handler(const hstack_t hstack)
 {
-
+    if (hstack >= 0 && hstack <= g_table.lastStack && (g_table.pTable[hstack].stackSize != -1))
+        return 1;
+    else
+        return 0;
 }
 
 unsigned int stack_size(const hstack_t hstack)
@@ -78,5 +96,5 @@ void stack_push(const hstack_t hstack, const void *data_in, const unsigned int s
 
 unsigned int stack_pop(const hstack_t hstack, void *data_out, const unsigned int size)
 {
-    
+
 }
