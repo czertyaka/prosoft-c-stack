2297c01e04428be42eaaac77dcf67730f2dec833
igor66ru <igor-chell@yandex.ru>
2025-11-11 06:20:47 +0500

    add functions description

--- cstack.c
+++ cstack.c
@@ -5,38 +5,133 @@
 
 hstack_t stack_new(void)
 {
-    return -1;
+    struct stackB_t* newStack = malloc(sizeof(struct stackB_t));
+    if(newStack == NULL) return -1;
+    if(laststack != 0)
+    {
+        newStack->hstack = laststack->hstack + 1;
+        newStack->prev = laststack;
+    }
+    else
+    {
+        newStack->prev = NULL;
+        newStack->hstack = 1;
+    }
+    newStack->stackLast = NULL;
+    laststack = newStack;
+    return laststack->hstack;
 }
 
 void stack_free(const hstack_t hstack)
 {
-    UNUSED(hstack);
+    if(laststack == NULL) return;
+    struct stackB_t* stackDel = NULL;
+    if(laststack->hstack == hstack)
+    {
+        stackDel = laststack;
+        laststack = stackDel->prev;
+    }
+    else
+    {
+        if(laststack->prev == NULL) return;
+        struct stackB_t* stackPrev = laststack;
+        struct stackB_t* stackCurrent = laststack->prev;
+        do
+        {
+            if(stackCurrent->hstack == hstack)
+            {
+                stackPrev->prev = stackCurrent->prev;
+                stackDel = stackCurrent;
+                break;
+            }
+            stackPrev = stackCurrent;
+            stackCurrent = stackCurrent->prev;
+        }
+        while(stackCurrent != NULL);
+    }
+    if(stackDel == NULL) return;
+    if(stackDel->stackLast != NULL) freeElem(stackDel->stackLast);
+    free(stackDel);
+}
+
+void freeElem(struct stackElem_t* stackElem)
+{
+    if(stackElem->prev != NULL) freeElem(stackElem->prev);
+    free(stackElem);
 }
 
 int stack_valid_handler(const hstack_t hstack)
 {
-    UNUSED(hstack);
-    return 1;
+    struct stackB_t* stackCurrent = stackB_search(hstack);
+    return stackCurrent == NULL ? 1 : 0;
 }
 
 unsigned int stack_size(const hstack_t hstack)
 {
-    UNUSED(hstack);
-    return 0;
+    struct stackB_t* stackCurrent = stackB_search(hstack);
+    if(stackCurrent == NULL) return 0;
+    struct stackElem_t* stackElemCurrent = stackCurrent->stackLast;
+    unsigned int count = 0;
+    while(stackElemCurrent != NULL)
+    {
+        stackElemCurrent = stackElemCurrent->prev;
+        count++;
+    }
+    return count;
 }
 
 void stack_push(const hstack_t hstack, const void* data_in, const unsigned int size)
 {
-    UNUSED(hstack);
-    UNUSED(data_in);
-    UNUSED(size);
+    if(data_in == NULL || size == 0) return;
+    struct stackB_t* stackCurrent = stackB_search(hstack);
+    if(stackCurrent == NULL) return;
+    struct stackElem_t* newStackElem = malloc(sizeof(struct stackElem_t) + size);
+    if(newStackElem == NULL) return;
+    char* data = newStackElem->data;
+    const char* data_in_p = data_in;
+    while(data != newStackElem->data + size && data_in_p != data_in + size)
+    {
+        *data = *data_in_p;
+        data++;
+        data_in_p++;
+    }
+    newStackElem->size = size;
+    newStackElem->prev = stackCurrent->stackLast == NULL ? NULL : stackCurrent->stackLast;
+    stackCurrent->stackLast = newStackElem;
 }
 
 unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int size)
 {
-    UNUSED(hstack);
-    UNUSED(data_out);
-    UNUSED(size);
-    return 0;
+    if(data_out == NULL || size == 0) return 0;
+    struct stackB_t* stackCurrent = stackB_search(hstack);
+    if(stackCurrent == NULL) return 0;
+    unsigned int count = 0;
+    struct stackElem_t* stackElemCurrent = stackCurrent->stackLast;
+    if(stackElemCurrent == NULL) return 0;
+    if(stackElemCurrent->size != size) return 0;
+    char* data = stackElemCurrent->data;
+    char* data_out_p = data_out;
+    while(data != stackElemCurrent->data + size && data_out_p != data_out + size)
+    {
+        *data_out_p = *data;
+        data++;
+        data_out_p++;
+        count++;
+    }
+    stackCurrent->stackLast = stackElemCurrent->prev;
+    free(stackElemCurrent);
+    return count;
+}
+
+struct stackB_t* stackB_search(const hstack_t hstack)
+{
+    if(laststack == NULL) return NULL;
+    struct stackB_t* stackCurrent = laststack;
+    while(stackCurrent != NULL)
+    {
+        if(stackCurrent->hstack == hstack) return stackCurrent;
+        stackCurrent = stackCurrent->prev;
+    }
+    return NULL;
 }
 
--- cstack.h
+++ cstack.h
@@ -1,7 +1,28 @@
 #ifndef CSTACK_H
 #define CSTACK_H
 
+#include <stdlib.h>
+
 typedef int hstack_t;
+
+struct stackElem_t
+{
+    struct stackElem_t* prev;
+    unsigned int size;
+    char data[0];
+};
+
+struct stackB_t
+{
+    struct stackB_t* prev;
+    struct stackElem_t* stackLast;
+    hstack_t hstack;
+};
+
+static struct stackB_t* laststack = NULL;
+
+struct stackB_t* stackB_search(const hstack_t hstack);
+void freeElem(struct stackElem_t* stackElem);
 
 hstack_t stack_new(void);
 void stack_free(const hstack_t stack);
